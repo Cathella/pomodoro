fa37b68bba99591b380b6bb94ed6d3b80b0f4252
Cathella <ncathie214@gmail.com>
2022-01-17 05:16:49 +0300

    App setup, Add state

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ timeLeft, setTimeLeft ] = useState(25 * 60);
+
+  const minutes = Math.floor(timeLeft / 60);
+  const seconds = (timeLeft - minutes * 60).toString().padStart(2, '0');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h2>Pomodoro!</h2>
+
+      <div className="timer">
+        <span>{minutes}</span>
+        <span>:</span>
+        <span>{seconds}</span>
+      </div>
+
+      <div className="buttons">
+        <button>Start</button>
+        <button>Stop</button>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
